1d92ec9dfa7a724e1d22c6e1187a66491ee60db2
Axel Castells Monllau <axelcastellsmonllau@gmail.com>
2018-11-15 13:14:48 +0100

    Project Setup

---
+++ Core/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vortex
+{
+    public class CoreTester
+    {
+        public static string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
