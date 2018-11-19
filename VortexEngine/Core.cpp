d8378a401353397ea6ae8bd3f86b77d09a063e6e
Axel Castells Monllau <axelcastellsmonllau@gmail.com>
2018-11-19 22:18:46 +0100

    Static Link Library setup done. GLFW3 added to VortexEngine.

---
+++ VortexEngine/Core.cpp
@@ -0,0 +1,15 @@
+#include "Core.h"
+#include <GLFW\glfw3.h>
+
+using namespace Vortex;
+
+void VCore::Init() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+std::string VCore::HelloWorld() {
+	return (char*)"Hello Patitas!";
+}
---
+++ VortexEngine/Core.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+namespace Vortex 
+{
+	static class VCore 
+	{
+	public:
+		VCore();
+
+		static void Init();
+
+		static void PabloEscobar();
+
+		static std::string HelloWorld();
+	};
+}
